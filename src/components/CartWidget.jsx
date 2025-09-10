56b2569d56b3ae1547ae4eaf20866b37996cd355
VALUSIS <fernandezpvalentin22@gmail.com>
2025-09-10 13:22:01 -0300

    cart

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,7 +1,10 @@
+import carrito from "../assets/carrito.png";
+
 function CartWidget() {
   return (
     <div>
-      🛒 <span>0</span>
+      <img src={carrito} alt="Carrito" style={{ height: "50px", marginRight: "10px"}} />
+      <span>0</span>
     </div>
   )
 }
